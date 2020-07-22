e18d66917332ea00f920cf17e798ecd1215c4140
WayJam So <imsuwj@gmail.com>
2020-07-22 23:35:26 +0800

    format code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
   const backToTopBtn = document.getElementById("back_to_top");
 
-  window.onscroll = function() {
+  window.onscroll = function () {
     scrollFunction(backToTopBtn);
   };
 
-	backToTopBtn.addEventListener("click", backToTop)
+  backToTopBtn.addEventListener("click", backToTop);
 })();
 
 function scrollFunction(btn) {
@@ -16,7 +16,7 @@ function scrollFunction(btn) {
 
 // When the user clicks on the button, scroll to the top of the document
 function backToTop(e) {
-	e.preventDefault()
+  e.preventDefault();
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
